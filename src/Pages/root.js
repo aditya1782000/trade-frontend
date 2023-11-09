1802bd4ac261886fb44bc500bd74c0424cdec8f7
aditya1782000 <97563832+aditya1782000@users.noreply.github.com>
2023-11-09 12:58:20 +0530

    Update root.js

--- src/Pages/root.js
+++ src/Pages/root.js
@@ -1,15 +1,23 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../components/Navigation";
+import classes from "./Root.module.css";
 
-function Root () {
-    return (
-        <>
-        <Navigation />
-        <main>
-            <Outlet />
-        </main>
-        </>
-    );
-};
+function Root() {
+  return (
+    <>
+      <Navigation />
+      <main>
+        <Outlet />
+        <div className={classes.description}>
+        <h1 className={classes.h1}>Welcome to Niner Traders</h1>
+            <p className={classes.p}>Niner Traders is the trading platform for trading 
+                electronic devices like mobile phones, tablets, laptops, 
+                headphones, moniters, and many more. Niner Traders will 
+                try to make your expreince smooth in the journey of electronic treding. </p>
+    </div>
+      </main>
+    </>
+  );
+}
 
 export default Root;
